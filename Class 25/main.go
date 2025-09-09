bde2a82ecea3c88a2f1f94ae89a57424efdfd535
Aishwary Gathe <aishwarygathe@gmail.com>
2025-09-09 19:00:25 +0530

    Updating class 25

--- Class 25/main.go
+++ Class 25/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 //model for course -file
@@ -45,6 +47,22 @@ func getAllCourse(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Get all Courses")
 	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOnecourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type","application/json")
+
+	params := mux.Vars(r)
+	fmt.Println(params)
+
+	for _, course := range courses{
+		if course.CourseId == params["id"]{
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Course foudn with given id")
 }
 
 
