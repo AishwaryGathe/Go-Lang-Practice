68659c34485a38831e80e94e3094fc574b8635cc
Aishwary Gathe <aishwarygathe@gmail.com>
2025-09-16 19:29:24 +0530

    updating Class 25

--- Class 25/main.go
+++ Class 25/main.go
@@ -3,33 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
-//model for course -file
-type Course struct{
-	CourseId string `json:"courseid"`
-	CourseName string `json:"coursename"`
-	CoursePrice int  `json:"price"`
-	Author *Author `json:"author"`	
+// model for course -file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
 }
 
-type Author struct{
+type Author struct {
 	Fullname string `json:"fullname"`
-	Website string `json:"website"`
+	Website  string `json:"website"`
 }
 
-
-
-//fake Database
+// fake Database
 var courses []Course
 
 //middleware -file
 
 func (c *Course) Isempty() bool {
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
@@ -37,33 +39,57 @@ func main() {
 
 }
 
-//controllers - file
+// controllers - file
 // serve home route
-func serveHome(w http.ResponseWriter , r *http.Request){
+func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to APi by aishwary</h1>"))
 }
 
-func getAllCourse(w http.ResponseWriter, r *http.Request){
+func getAllCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all Courses")
 	w.Header().Set("Content-type", "application/json")
 	json.NewEncoder(w).Encode(courses)
 }
 
-func getOnecourse(w http.ResponseWriter, r *http.Request){
+func getOnecourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get one course")
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
 	fmt.Println(params)
 
-	for _, course := range courses{
-		if course.CourseId == params["id"]{
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
 			json.NewEncoder(w).Encode(course)
 			return
 		}
 	}
 	json.NewEncoder(w).Encode("No Course foudn with given id")
+	return
 }
 
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
 
+	//what if: body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data!!")
+	}
 
+	//what about - {}
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.Isempty() {
+		json.NewEncoder(w).Encode("No data in Json!!")
+		return
+	}
+	// generate unique id, string
+	// append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+}
