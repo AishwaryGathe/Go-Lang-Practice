cfaaf70e29d147b32e27b9ee51f3d5bef80d48a0
Aishwary Gathe <aishwarygathe@gmail.com>
2025-09-16 20:13:32 +0530

    updating class 25

--- Class 10/main.go
+++ Class 10/main.go
@@ -7,7 +7,6 @@ import (
 func main(){
 	var courses = []string{"reactjs", "java", "python","go", "Javascript"}
 	fmt.Println(courses)
-
 	var index int = 2
 	courses = append(courses[:index], courses[index+1:]...)
 	fmt.Println(courses)
