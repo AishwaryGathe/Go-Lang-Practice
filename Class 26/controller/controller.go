9a7bf882941064a26b7233477533e8d5926012bd
Aishwary Gathe <aishwarygathe@gmail.com>
2025-09-18 20:21:30 +0530

    updating class26

--- Class 26/controller/controller.go
+++ Class 26/controller/controller.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/AishwaryGathe/mongoapi/model"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -33,4 +34,16 @@ func init() {
 
 	fmt.Println("Collection instance is ready!!")
 
+}
+
+
+// mongodb helpers - files
+
+func insertOneMovie(movie model.Netflix){
+	inserted, err := collection.InsertOne(context.Background(),movie)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted 1 Movie in db with id:", inserted.InsertedID)
 }
