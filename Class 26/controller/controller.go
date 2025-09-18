87b5346fef5bcfa7c160573af4fca3203497eac9
Aishwary Gathe <aishwarygathe@gmail.com>
2025-09-18 20:09:53 +0530

    updateing class26

---
+++ Class 26/controller/controller.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = "mongodb+srv://admin:admin123@cluster0.o2i0gao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const dbName = "netflix"
+const colName = "watchlist"
+
+var collection *mongo.Collection
+
+//connect with database mongodb
+
+func init() {
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB Connected Sucessfully!!!")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	fmt.Println("Collection instance is ready!!")
+
+}
